d2e7fcad0ff38f557b458841dda8a597e713b877
Fahmudul <fahmudul7890@gmail.com>
2025-04-17 14:34:11 +0600

    Feat
    Added data base Tune Trails and hosted in Render, Added Table Customer, Bike, ServiceRecord
    
    Added catchAsync, sendResponse utility function

---
+++ src/app/Utils/sendResponse.ts
@@ -0,0 +1,20 @@
+import { Response } from "express";
+interface IResponse<T> {
+  success: boolean;
+  statusCode: number;
+  message: string;
+  meta?: {
+    page: number;
+    limit: number;
+    total: number;
+  };
+  data: T;
+}
+export const sendResponse = <T>(res: Response, data: IResponse<T>) => {
+  const { statusCode, success, message, data: responseData } = data;
+  res.status(statusCode).json({
+    success,
+    message,
+    data: responseData,
+  });
+};
